c827da2e8a7829015b3351eee06621cd63ffc839
Bunnybean <45402108+Bunnybean@users.noreply.github.com>
2020-07-21 17:57:37 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -154,6 +154,12 @@ app.post("/create", function(req, resp){
    resp.redirect("/" + createNewList)
 });
 
-app.listen(3000, function(){
-    console.log("Server started on port 3000");
+//heroku
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function(){
+    console.log("Server started successfully");
 });
